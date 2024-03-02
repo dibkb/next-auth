01834034a9eb6a3e411c27827d9a96703791d111
dibkb <dibas9110@gmail.com>
2024-03-02 12:37:51 +0530

    login page

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Image from "next/image";
+import { imagesLink } from "@/utils/images";
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="flex flex-col h-full mt-6 px-6">
       <div className="h-56 relative">
         <Image
-          src="https://images.unsplash.com/flagged/photo-1572392640988-ba48d1a74457?w=1400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fHBhaW50aW5nfGVufDB8fDB8fHww"
+          src={imagesLink.login}
           fill
           objectFit="cover"
           alt="Picture of the author"
---
+++ utils/images.tsx
@@ -0,0 +1,4 @@
+export const imagesLink: Record<string, string> = {
+  login:
+    "https://images.unsplash.com/flagged/photo-1572392640988-ba48d1a74457?w=1400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fHBhaW50aW5nfGVufDB8fDB8fHww",
+};
