c4c369b8341e8e42f3c6b9900c3225e522ef196e
dibkb <dibas9110@gmail.com>
2024-03-02 12:06:33 +0530

    next font

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,19 @@
 import { LoginButton } from "@/components/auth/login-button";
 import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { Poppins } from "next/font/google";
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["600"],
+});
 export default function Home() {
   return (
     <main className="flex h-full flex-col items-center justify-center">
       <div className="w-full max-w-md flex flex-col gap-y-6 text-center">
-        <h2 className="text-5xl">🎃 Authzone</h2>
+        <h2 className={cn("text-5xl font-semibold", font.className)}>
+          🎃 Authzone
+        </h2>
         <p className="text-sm text-slate-400">
           A simple authentication service
         </p>
