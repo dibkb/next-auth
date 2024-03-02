c7d826c71430ce62a7cc69ac605787662701a3b8
dibkb <dibas9110@gmail.com>
2024-03-02 16:39:33 +0530

    color icons

--- components/auth/socail.tsx
+++ components/auth/socail.tsx
@@ -1,15 +1,15 @@
 "use client";
-
-import { FaGithub, FaGoogle } from "react-icons/fa";
+import { FcGoogle } from "react-icons/fc";
+import { FaGithub } from "react-icons/fa";
 import { Button } from "../ui/button";
 
 export const Social = () => {
   return (
     <div className="flex items-center w-full gap-x-4">
-      <Button variant={"outline"} size="lg" className="w-full">
-        <FaGoogle className="h-5 w-5" />
+      <Button variant={"outline"} size="lg" className="w-full shadow-none">
+        <FcGoogle className="h-5 w-5" />
       </Button>
-      <Button variant={"outline"} size="lg" className="w-full">
+      <Button variant={"outline"} size="lg" className="w-full shadow-none">
         <FaGithub className="h-5 w-5" />
       </Button>
     </div>
