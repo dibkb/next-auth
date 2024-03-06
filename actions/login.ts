810b0102a9b26d2c2455bff0213a2aa8dc451de2
dibkb <dibas9110@gmail.com>
2024-03-06 10:59:17 +0530

    register route

--- actions/login.ts
+++ actions/login.ts
@@ -1,7 +1,10 @@
 "use server";
+import { db } from "@/lib/db";
 import * as z from "zod";
 import { loginSchema, registerSchema } from "@/schemas";
 import bcrypt from "bcrypt";
+import { getUserByEmail } from "@/utils/user";
+import { error } from "console";
 export const register = async (values: z.infer<typeof registerSchema>) => {
   const serverValid = registerSchema.safeParse(values);
   if (!serverValid.success) {
@@ -17,10 +20,22 @@ export const register = async (values: z.infer<typeof registerSchema>) => {
       error: "Email already in use",
     };
   }
-  // create user
-  return {
-    success: "Welcome back",
-  };
+  const newUser = await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashPw,
+    },
+  });
+  if (newUser) {
+    return {
+      success: `Account successfully created for ${newUser.image}`,
+    };
+  } else {
+    return {
+      error: `Account could not be created`,
+    };
+  }
 };
 export const login = async (values: z.infer<typeof loginSchema>) => {
   const serverValid = loginSchema.safeParse(values);
